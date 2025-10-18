cbd6ce423a5aed361c0db72f829a614c5cbe62f0
Harsh Narwariya <h.narwariya@thesynapses.com>
2025-10-18 22:26:13 +0530

    allowed hosts

--- arsongsapi/settings.py
+++ arsongsapi/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from config import CONFIG
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-sl5qdnibcbkpck#z&ia1i)t-!84(t9v367$(krz)6z+_ihm7#c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = CONFIG.get("ALLOWED_HOSTS", [])
 
 
 # Application definition
