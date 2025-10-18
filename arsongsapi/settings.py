62a48aef38ff9a62b90dd1c02291fc9ab7ffdc0c
Harsh Narwariya <h.narwariya@thesynapses.com>
2025-10-18 22:35:52 +0530

    static url

--- arsongsapi/settings.py
+++ arsongsapi/settings.py
@@ -121,7 +121,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = CONFIG.get("STATIC_URL", '/static/')
+STATIC_ROOT = CONFIG.get("COLLECT_STATIC_PATH", BASE_DIR / 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
