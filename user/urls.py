ed705fe263b76459c4126c714ceec877698da0c8
Harsh Narwariya <h.narwariya@thesynapses.com>
2026-04-25 12:28:24 +0530

    uv added

--- user/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import LoginAPIView, UserProfileAPIView
-
-urlpatterns = [
-    path("login/", LoginAPIView.as_view(), name="login"),
-    path("profile/", UserProfileAPIView.as_view(), name="profile"),
-]
